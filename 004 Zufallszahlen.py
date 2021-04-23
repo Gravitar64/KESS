957356d278becc728d350bbc6431d324823a9369
Gravitar64 <gravitar@web.de>
2021-04-23 16:49:11 +0200

    randint

--- 004 Zufallszahlen.py
+++ 004 Zufallszahlen.py
@@ -2,7 +2,7 @@ import random
 
 anz_sechser = 0
 for i in range(100):
-  würfel = random.randrange(1,7)
+  würfel = random.randint(1,6)
   if würfel == 6:
     anz_sechser += 1
 print(anz_sechser)    
